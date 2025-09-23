f84a2ee02c780684112a92ceb906f5d58604c79b
albertofavaro <alberto.f@faculty.ai>
2025-09-23 17:08:16 +0200

    Add temperature parameter. Add please hold print statement whilst calling openai. Add comments to main script.

--- a_journalist_of_real_character/src/a_journalist_of_real_character/main.py
+++ a_journalist_of_real_character/src/a_journalist_of_real_character/main.py
@@ -18,6 +18,7 @@ from read_parse_write import read_and_parse_methodology_sources
 from read_parse_write import read_and_parse_transcript_sources
 from engineer_prompt import assemble_prompt
 
+# Inputs are kept here for users to customise the Journalist.
 CONFIG = {
     "methodology_sources": [
         {
@@ -39,29 +40,36 @@ CONFIG = {
     "transcript_sources_directory_path": "../../../transcript",
     "prompt_skeleton_filepath": "../../prompt_skeleton/prompt_skeleton.md",
     "model": "gpt-5-2025-08-07",
-    "reasoning": {"effort": "low"},
+    "reasoning": {"effort": "medium"},
+    "temperature": 1,
     "text": {"verbosity": "medium"},
 }
 
 
 def main():
+    # Read in, and parse all sources.
     methodology_sources = read_and_parse_methodology_sources(CONFIG)
     transcript_sources = read_and_parse_transcript_sources(
         CONFIG["transcript_sources_directory_path"]
     )
 
+    # Read in the prompt skeleton.
     with open(CONFIG["prompt_skeleton_filepath"], "r") as f:
         prompt_skeleton_str = f.read()
 
+    # Assemble the prompt.
     prompt_str = assemble_prompt(
         prompt_skeleton_str, methodology_sources, transcript_sources
     )
 
+    # Call the LLM.
     client = OpenAI()
+    print("Calling OpenAI, please hold.")
     result = client.responses.create(
         model=CONFIG["model"],
         input=prompt_str,
         reasoning=CONFIG["reasoning"],
+        temperature=CONFIG["temperature"],
         text=CONFIG["text"],
     )
     print(result.output_text)
